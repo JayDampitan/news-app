fc2f46d41a7cdb2c9513cc54139dd5b2bf2335c5
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-08-26 10:58:13 -0700

    feat(movies-article): create Movies component with interface and styled-components

--- the-toad-tribune/src/layout/Movies.tsx
+++ the-toad-tribune/src/layout/Movies.tsx
@@ -1,11 +1,52 @@
 import styled from "styled-components";
+import { NewsResponse } from "../api/newsApi";
+import { usePagination } from "../hooks";
 
-const Movies = () => {
-  return(
+interface MoviesProps {
+  moviesArticle: NewsResponse;
+}
+
+const Movies: React.FC<MoviesProps> = ({ moviesArticle }) => {
+  const [onNextButton, onPrevButton, pageNumber, startBeginning, startEnd] =
+    usePagination();
+
+  const article = moviesArticle.articles?.[pageNumber];
+
+  return moviesArticle.articles.length ? (
     <MoviesStyles>
-     Movies
-    </MoviesStyles>
+      <button
+        onClick={() => {
+          moviesArticle.articles.length - 1 === pageNumber
+            ? startBeginning()
+            : onNextButton();
+        }}
+      >
+        Next Button
+      </button>
 
+      <button
+        onClick={() => {
+          pageNumber === 0
+            ? startEnd(moviesArticle.articles.length - 1)
+            : onPrevButton();
+        }}
+      >
+        Previous Button
+      </button>
+
+      <div>
+        {article?.title}
+
+        {article?.author}
+
+        {article?.publishedAt}
+
+        {article?.description}
+      </div>
+      <Image src={article.urlToImage} />
+    </MoviesStyles>
+  ) : (
+    <p>Loading....</p>
   )
 };
 
@@ -16,3 +57,9 @@ const MoviesStyles = styled.div`
   grid-area: 6/6/10/9;
   margin-bottom: .5em;
 `
+
+const Image = styled.img`
+  height: 100%;
+  width: 100%;
+  overflow: none;
+`;
