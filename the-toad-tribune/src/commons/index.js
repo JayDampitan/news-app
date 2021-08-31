d76647bf109366bd2ea41e413209c593a707e82d
JayDampitan <jaykennethpd@gmail.com>
2021-08-31 14:50:35 -0700

    commit before pull

---
+++ the-toad-tribune/src/commons/index.js
@@ -0,0 +1,23 @@
+import {
+    ArticleContentContainer,
+    HeaderContainer,
+    TitleContainer,
+    AuthorContainer,
+    PublishedAtContainer,
+    ArticleDescriptionContainer,
+    ImageContainer
+} from './articleDesign';
+
+import { Buttons } from './buttons';
+
+
+export {
+    ArticleContentContainer,
+    HeaderContainer,
+    TitleContainer,
+    AuthorContainer,
+    PublishedAtContainer,
+    ArticleDescriptionContainer,
+    ImageContainer,
+    Buttons,
+};
