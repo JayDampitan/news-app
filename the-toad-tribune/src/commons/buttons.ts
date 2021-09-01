3256a44a262e736b0a91768beafba19f18ce8995
JayDampitan <jaykennethpd@gmail.com>
2021-09-01 11:09:45 -0700

    feat(movie-article): create layout for movie article using styled components to avoid overflow.

--- the-toad-tribune/src/commons/buttons.ts
+++ the-toad-tribune/src/commons/buttons.ts
@@ -1,27 +1,25 @@
-import styled from 'styled-components';
-
+import styled from "styled-components";
 
 export const Buttons = styled.button`
-    height: 1.7rem;
-    width: 1.7rem;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    opacity: 0;
-    border: none;
-    background-color: #fff;
-    border-radius: 50%;
-    margin: .2rem;
- 
-        img {
-            height: 1.2rem;
-            width: 1.2rem;
-            color: #fff;
-        }
+  height: 1.7rem;
+  width: 1.7rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  opacity: 0;
+  border: none;
+  background-color: #fff;
+  border-radius: 50%;
+  margin: 0.1rem;
 
-    :hover {
-        opacity: 1;
-        transition: .7s;
-    }
+  img {
+    height: 1.2rem;
+    width: 1.2rem;
+    color: #fff;
+  }
+
+  :hover {
+    opacity: 1;
+    transition: 0.7s;
+  }
 `;
-
